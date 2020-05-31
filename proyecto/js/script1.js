2e42b73e383c8dbfe6d5cd82f11660d389d32eed
Benigno Cantero Martinez <benigno.cantero@gmail.com>
2020-05-31 19:59:22 +0200

    paginas terminadas

--- proyecto/js/script1.js
+++ proyecto/js/script1.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
-    // AGREGANDO CLASE ACTIVE AL PRIMER ENLACE ====================
+    // AGREGANDO CLASE ACTIVE AL PRIMER ENLACE 
     $('.category_list .category_item[category="all"]').addClass('ct_item-active');
 
-    // FILTRANDO PRODUCTOS  ============================================
+    // FILTRANDO PRODUCTOS  
 
     $('.category_item').click(function() {
         var catProduct = $(this).attr('category');
@@ -13,7 +13,7 @@ $(document).ready(function() {
         $('.category_item').removeClass('ct_item-active');
         $(this).addClass('ct_item-active');
 
-        // OCULTANDO PRODUCTOS =========================
+        // OCULTANDO PRODUCTOS 
         $('.product-item').css('transform', 'scale(0)');
 
         function hideProduct() {
@@ -21,7 +21,7 @@ $(document).ready(function() {
         }
         setTimeout(hideProduct, 400);
 
-        // MOSTRANDO PRODUCTOS =========================
+        // MOSTRANDO PRODUCTOS 
         function showProduct() {
             $('.product-item[category="' + catProduct + '"]').show();
             $('.product-item[category="' + catProduct + '"]').css('transform', 'scale(1)');
@@ -29,7 +29,7 @@ $(document).ready(function() {
         setTimeout(showProduct, 400);
     });
 
-    // MOSTRANDO TODOS LOS PRODUCTOS =======================
+    // MOSTRANDO TODOS LOS PRODUCTOS
 
     $('.category_item[category="all"]').click(function() {
         function showAll() {
